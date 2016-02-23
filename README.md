a5632df6051f4920848f2cb0313b1053e5529daf
Luke Kreczko <lkreczko@googlemail.com>
2016-02-23 14:48:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# puppet-virtual_organisation
---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# Class: virtual_organization
+#
+# This module manages virtual_organization
+#
+# Parameters: none
+#
+# Actions:
+#
+# Requires: see Modulefile
+#
+# Sample Usage:
+#
+class virtual_organization {
+
+}
